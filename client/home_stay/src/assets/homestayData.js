dafe50df22750106d59d63f5e806a3c3991161b5
Kuldeep Panwar <kuldeeppanwar460@gmail.com>
2023-03-29 13:29:02 +0530

    third commit

---
+++ client/home_stay/src/assets/homestayData.js
@@ -0,0 +1,106 @@
+import img1 from './image-1.jpg'
+import img2 from './image-2.jpg'
+import img3 from './image-3.jpg'
+import img4 from './image-4.jpg'
+import img5 from './image-5.jpg'
+import img6 from './image-7.jpg'
+const homestays = [
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"231 gunjan bawan, ganesh nagar, indore MP",
+        rooms:8,
+        capacity:14,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:1500,
+            double:2500,
+            extra:1100
+        }
+
+    },
+
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"420 raj bawan, kalani nagar, bhopal MP",
+        rooms:5,
+        capacity:8,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:3500,
+            double:6000,
+            extra:3000
+        }
+
+    }
+    ,
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"888 kamala bawan, rajesh nagar, delhi MP",
+        rooms:5,
+        capacity:15,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:800,
+            double:1500,
+            extra:700
+        }
+
+    },
+
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"231 rani bhag, vijay nagar, indore MP",
+        rooms:8,
+        capacity:14,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:2000,
+            double:3500,
+            extra:1500
+        }
+
+    },
+
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"231 rani bhag, vijay nagar, indore MP",
+        rooms:8,
+        capacity:14,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:2000,
+            double:3500,
+            extra:1500
+        }
+
+    }
+    ,
+    {
+        about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero iste modi praesentium voluptate optio sint, alias magnam temporibus amet similique?",
+        images: [img1 ,img2, img3, img4,img5,img6],
+        location:"231 rani bhag, vijay nagar, indore MP",
+        rooms:8,
+        capacity:14,
+        facility:['parking','internet','hot water','pick and drop','washing machine','power backup'],
+        food:'simple homecooked food like rice ,capati, dal , poha, rajma',
+        price:{
+            single:2000,
+            double:3500,
+            extra:1500
+        }
+
+    }
+    
+]
+
+export default homestays;
--- client/home_stay/src/components/Homestays.js
+++ client/home_stay/src/components/Homestays.js
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Homestays.module.css";
+import homestays from "../assets/homestayData";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import Aboutus from "./Aboutus";
+import { useNavigate } from "react-router-dom";
 export default function Homestays() {
+  const [homestay, setHomestay] = useState([]);
+  const navigate = useNavigate();
+
+  const handleClick =()=>{
+    navigate('/about');
+  }
+
+  const fetchHomestay = () => {
+    setHomestay(homestays);
+  };
+  useEffect(()=>{
+    fetchHomestay();
+  },[]);
+
   return (
     <div>
-      <h2 style={{ marginTop: "120px" }}>homestay page</h2>
+      {/* <h2 style={{ marginTop: "120px" }}>homestay page</h2> */}
+      <h2 className={styles.headings}>Homestays</h2>
+      <Container>
+        <Row>
+          {homestay.map((h) => {
+            return (
+              <Col sm="12" md="6" lg="4">
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={h.images[0]} />
+                  <Card.Body>
+                    <Card.Title>price : {h.price.single}</Card.Title>
+                    <Card.Text>
+                      {
+                        h.location
+                      }
+                    </Card.Text>
+                    
+                    <Button variant="primary" onClick={()=>{handleClick()}}>View</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
